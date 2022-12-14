54b542c678dee89007a2ac2ecbdfd87453eb142f
Adam Gibson <adamsteven15@gmail.com>
2022-12-14 13:43:24 +0000

    Added stats and better artwork

--- src/components/PokemonStatBlock.tsx
+++ src/components/PokemonStatBlock.tsx
@@ -1,6 +1,7 @@
-import { Button, Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import { useState, useEffect } from "react";
 import { pokemonStatBlock } from "../interfaces/pokemonStatBlock";
+import _ from "lodash";
 
 export const PokemonStatBlock = () => {
   const [pokemon, setPokemon] = useState<pokemonStatBlock | undefined>(
@@ -24,23 +25,34 @@ export const PokemonStatBlock = () => {
   }, []);
 
   return (
-    <Container>
+    <Container sx={{ height: "80vh" }}>
       <Grid container spacing={2} direction="row">
         <Grid item xs={6}>
           <img
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon?.id}.png`}
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`}
+            height={"300rem"}
             alt=""
           />
         </Grid>
-
         <Grid item xs={6}>
-          <Button
-            onClick={() => {
-              console.log(pokemon);
-            }}
-          >
-            Click me
-          </Button>
+          <Typography variant="h1">{_.capitalize(pokemon?.name)}</Typography>
+          <Typography variant="subtitle1">
+            Type:{" "}
+            {pokemon &&
+              pokemon.types?.map((type, index) => (
+                <span key={`${type}_${index}`}>
+                  {" "}
+                  {_.capitalize(type.type.name)}
+                </span>
+              ))}
+          </Typography>
+
+          {pokemon &&
+            pokemon.stats?.map((stat, index) => (
+              <Typography key={`${stat}_${index}`} variant="subtitle1">
+                {_.capitalize(stat.stat.name)}: {stat.base_stat}
+              </Typography>
+            ))}
         </Grid>
       </Grid>
     </Container>
