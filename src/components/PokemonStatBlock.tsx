56be210ed99fd92337d5e07a7c6980257414063b
Adam Gibson <adamsteven15@gmail.com>
2022-12-19 16:17:38 +0000

    Converted to graphql request

--- src/components/PokemonStatBlock.tsx
+++ src/components/PokemonStatBlock.tsx
@@ -1,22 +1,34 @@
 import { Box, Grid, Typography } from "@mui/material";
 import { useState, useEffect, useContext } from "react";
-import { pokemonStatBlock } from "../interfaces/pokemonStatBlock";
+import { Root } from "../interfaces/pokemonStatBlock";
+import { pokemonContext } from "../Providers/PokemonProvider";
 import Image from "mui-image";
 import _ from "lodash";
-import { pokemonContext } from "../Providers/PokemonProvider";
 
 export const PokemonStatBlock = () => {
-  const [pokemon, setPokemon] = useState<pokemonStatBlock | undefined>(
-    undefined
-  );
+  const [pokemon, setPokemon] = useState<Root | undefined>(undefined);
 
   const { currentPokemon, setCurrentPokemon } = useContext(pokemonContext);
 
   const fetchData = async (pokemonToSearch: number) => {
-    const pokemonFetch: Response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonToSearch}`
-    );
-    return pokemonFetch.json();
+    try {
+      const response = await fetch("https://beta.pokeapi.co/graphql/v1beta", {
+        method: "POST",
+        headers: {},
+        body: JSON.stringify({
+          query: `query samplePokeAPIquery {\n  pokemon_v2_pokemon(where: {id: {_eq: ${pokemonToSearch}}}) {\n    id\n    name\n    pokemon_v2_pokemonstats {\n          base_stat\n          pokemon_v2_stat {\n            name\n          }\n        }\n    pokemon_v2_pokemontypes {\n      pokemon_v2_type {\n        name\n      }\n    }\n  }\n}\n`,
+          variables: {},
+        }),
+      });
+
+      if (response.ok) {
+        let result = await response.json();
+        console.log(result);
+        return result;
+      }
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   useEffect(() => {
@@ -53,7 +65,7 @@ export const PokemonStatBlock = () => {
           <Image
             src={
               pokemon !== undefined
-                ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.id}.png`
+                ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon?.data.pokemon_v2_pokemon[0].id}.png`
                 : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png`
             }
             height="75%"
@@ -76,7 +88,7 @@ export const PokemonStatBlock = () => {
             gutterBottom
             variant="h1"
           >
-            {_.capitalize(pokemon?.name)}
+            {_.capitalize(pokemon?.data.pokemon_v2_pokemon[0].name)}
           </Typography>
         </Grid>
         <Grid item sm>
@@ -97,38 +109,42 @@ export const PokemonStatBlock = () => {
               Type:{" "}
             </Typography>
             {pokemon &&
-              pokemon.types?.map((type, index) => (
-                <Image
-                  key={`${type}_${index}`}
-                  src={`https://serebii.net/pokedex-bw/type/${type.type.name}.gif`}
-                  alt="Pokemon Type"
-                  width={"3em"}
-                  fit="contain"
-                  duration={1500}
-                  easing="linear"
-                  showLoading={true}
-                  errorIcon={true}
-                  shift={null}
-                  distance="100px"
-                  shiftDuration={900}
-                  bgColor="inherit"
-                />
-              ))}
+              pokemon.data.pokemon_v2_pokemon[0].pokemon_v2_pokemontypes?.map(
+                (type, index) => (
+                  <Image
+                    key={`${type}_${index}`}
+                    src={`https://serebii.net/pokedex-bw/type/${type.pokemon_v2_type.name}.gif`}
+                    alt="Pokemon Type"
+                    width={"3em"}
+                    fit="contain"
+                    duration={1500}
+                    easing="linear"
+                    showLoading={true}
+                    errorIcon={true}
+                    shift={null}
+                    distance="100px"
+                    shiftDuration={900}
+                    bgColor="inherit"
+                  />
+                )
+              )}
           </Box>
 
           {pokemon &&
-            pokemon.stats?.map((stat, index) => (
-              <Typography
-                key={`${stat}_${index}`}
-                variant="h1"
-                sx={{
-                  typography: { md: "h5", sm: "body1", xs: "subtitle1" },
-                  fontSize: { xl: "4rem", lg: "2rem", xs: "1rem" },
-                }}
-              >
-                {_.capitalize(stat.stat.name)}: {stat.base_stat}
-              </Typography>
-            ))}
+            pokemon.data.pokemon_v2_pokemon[0].pokemon_v2_pokemonstats?.map(
+              (stat, index) => (
+                <Typography
+                  key={`${stat}_${index}`}
+                  variant="h1"
+                  sx={{
+                    typography: { md: "h5", sm: "body1", xs: "subtitle1" },
+                    fontSize: { xl: "4rem", lg: "2rem", xs: "1rem" },
+                  }}
+                >
+                  {_.capitalize(stat.pokemon_v2_stat.name)}: {stat.base_stat}
+                </Typography>
+              )
+            )}
           {pokemon && (
             <Typography
               variant="h1"
@@ -138,7 +154,7 @@ export const PokemonStatBlock = () => {
               }}
             >
               Stat Total:{" "}
-              {pokemon.stats?.reduce(
+              {pokemon.data.pokemon_v2_pokemon[0].pokemon_v2_pokemonstats?.reduce(
                 (partialSum, a) => partialSum + a.base_stat,
                 0
               )}
--- src/interfaces/pokemonStatBlock.d.ts
+++ src/interfaces/pokemonStatBlock.d.ts
@@ -1,180 +1,31 @@
-﻿export interface pokemonStatBlock {
-  abilities?: (AbilitiesEntity)[] | null;
-  base_experience: number;
-  forms?: (AbilityOrFormsEntityOrVersionOrMoveLearnMethodOrVersionGroupOrMoveOrStatOrTypeOrSpecies)[] | null;
-  game_indices?: (GameIndicesEntity)[] | null;
-  height: number;
-  held_items?: (null)[] | null;
-  id: number;
-  is_default: boolean;
-  location_area_encounters: string;
-  moves?: (MovesEntity)[] | null;
-  name: string;
-  order: number;
-  past_types?: (null)[] | null;
-  species: AbilityOrFormsEntityOrVersionOrMoveLearnMethodOrVersionGroupOrMoveOrStatOrTypeOrSpecies;
-  sprites: Sprites;
-  stats?: (StatsEntity)[] | null;
-  types?: (TypesEntity)[] | null;
-  weight: number;
+﻿export interface Root {
+  data: Data
 }
-export interface AbilitiesEntity {
-  ability: AbilityOrFormsEntityOrVersionOrMoveLearnMethodOrVersionGroupOrMoveOrStatOrTypeOrSpecies;
-  is_hidden: boolean;
-  slot: number;
+
+export interface Data {
+  pokemon_v2_pokemon: PokemonV2Pokemon[]
 }
-export interface AbilityOrFormsEntityOrVersionOrMoveLearnMethodOrVersionGroupOrMoveOrStatOrTypeOrSpecies {
-  name: string;
-  url: string;
+
+export interface PokemonV2Pokemon {
+  id: number
+  name: string
+  pokemon_v2_pokemonstats: PokemonV2Pokemonstat[]
+  pokemon_v2_pokemontypes: PokemonV2Pokemontype[]
 }
-export interface GameIndicesEntity {
-  game_index: number;
-  version: AbilityOrFormsEntityOrVersionOrMoveLearnMethodOrVersionGroupOrMoveOrStatOrTypeOrSpecies;
+
+export interface PokemonV2Pokemonstat {
+  base_stat: number
+  pokemon_v2_stat: PokemonV2Stat
 }
-export interface MovesEntity {
-  move: AbilityOrFormsEntityOrVersionOrMoveLearnMethodOrVersionGroupOrMoveOrStatOrTypeOrSpecies;
-  version_group_details?: (VersionGroupDetailsEntity)[] | null;
+
+export interface PokemonV2Stat {
+  name: string
 }
-export interface VersionGroupDetailsEntity {
-  level_learned_at: number;
-  move_learn_method: AbilityOrFormsEntityOrVersionOrMoveLearnMethodOrVersionGroupOrMoveOrStatOrTypeOrSpecies;
-  version_group: AbilityOrFormsEntityOrVersionOrMoveLearnMethodOrVersionGroupOrMoveOrStatOrTypeOrSpecies;
+
+export interface PokemonV2Pokemontype {
+  pokemon_v2_type: PokemonV2Type
 }
-export interface Sprites {
-  back_default: string;
-  back_female?: null;
-  back_shiny: string;
-  back_shiny_female?: null;
-  front_default: string;
-  front_female?: null;
-  front_shiny: string;
-  front_shiny_female?: null;
-  other: Other;
-  versions: Versions;
-}
-export interface Other {
-  dream_world: DreamWorldOrIcons;
-  home: HomeOrOmegarubyAlphasapphireOrXYOrUltraSunUltraMoon;
-  officialArtwork: OfficialArtwork;
-}
-export interface DreamWorldOrIcons {
-  front_default: string;
-  front_female?: null;
-}
-export interface HomeOrOmegarubyAlphasapphireOrXYOrUltraSunUltraMoon {
-  front_default: string;
-  front_female?: null;
-  front_shiny: string;
-  front_shiny_female?: null;
-}
-export interface OfficialArtwork {
-  front_default: string;
-}
-export interface Versions {
-  generationi: Generationi;
-  generationii: Generationii;
-  generationiii: Generationiii;
-  generationiv: Generationiv;
-  generationv: Generationv;
-  generationvi: Generationvi;
-  generationvii: Generationvii;
-  generationviii: Generationviii;
-}
-export interface Generationi {
-  redblue: RedblueOrYellow;
-  yellow: RedblueOrYellow;
-}
-export interface RedblueOrYellow {
-  back_default: string;
-  back_gray: string;
-  back_transparent: string;
-  front_default: string;
-  front_gray: string;
-  front_transparent: string;
-}
-export interface Generationii {
-  crystal: Crystal;
-  gold: GoldOrSilver;
-  silver: GoldOrSilver;
-}
-export interface Crystal {
-  back_default: string;
-  back_shiny: string;
-  back_shiny_transparent: string;
-  back_transparent: string;
-  front_default: string;
-  front_shiny: string;
-  front_shiny_transparent: string;
-  front_transparent: string;
-}
-export interface GoldOrSilver {
-  back_default: string;
-  back_shiny: string;
-  front_default: string;
-  front_shiny: string;
-  front_transparent: string;
-}
-export interface Generationiii {
-  emerald: Emerald;
-  fireredleafgreen: FireredleafgreenOrRubysapphire;
-  rubysapphire: FireredleafgreenOrRubysapphire;
-}
-export interface Emerald {
-  front_default: string;
-  front_shiny: string;
-}
-export interface FireredleafgreenOrRubysapphire {
-  back_default: string;
-  back_shiny: string;
-  front_default: string;
-  front_shiny: string;
-}
-export interface Generationiv {
-  diamondpearl: DiamondpearlOrHeartgoldsoulsilverOrPlatinumOrAnimated;
-  heartgoldsoulsilver: DiamondpearlOrHeartgoldsoulsilverOrPlatinumOrAnimated;
-  platinum: DiamondpearlOrHeartgoldsoulsilverOrPlatinumOrAnimated;
-}
-export interface DiamondpearlOrHeartgoldsoulsilverOrPlatinumOrAnimated {
-  back_default: string;
-  back_female?: null;
-  back_shiny: string;
-  back_shiny_female?: null;
-  front_default: string;
-  front_female?: null;
-  front_shiny: string;
-  front_shiny_female?: null;
-}
-export interface Generationv {
-  blackwhite: Blackwhite;
-}
-export interface Blackwhite {
-  animated: DiamondpearlOrHeartgoldsoulsilverOrPlatinumOrAnimated;
-  back_default: string;
-  back_female?: null;
-  back_shiny: string;
-  back_shiny_female?: null;
-  front_default: string;
-  front_female?: null;
-  front_shiny: string;
-  front_shiny_female?: null;
-}
-export interface Generationvi {
-  omegarubyalphasapphire: HomeOrOmegarubyAlphasapphireOrXYOrUltraSunUltraMoon;
-  xy: HomeOrOmegarubyAlphasapphireOrXYOrUltraSunUltraMoon;
-}
-export interface Generationvii {
-  icons: DreamWorldOrIcons;
-  ultrasunultramoon: HomeOrOmegarubyAlphasapphireOrXYOrUltraSunUltraMoon;
-}
-export interface Generationviii {
-  icons: DreamWorldOrIcons;
-}
-export interface StatsEntity {
-  base_stat: number;
-  effort: number;
-  stat: AbilityOrFormsEntityOrVersionOrMoveLearnMethodOrVersionGroupOrMoveOrStatOrTypeOrSpecies;
-}
-export interface TypesEntity {
-  slot: number;
-  type: AbilityOrFormsEntityOrVersionOrMoveLearnMethodOrVersionGroupOrMoveOrStatOrTypeOrSpecies;
+
+export interface PokemonV2Type {
+  name: string
 }
